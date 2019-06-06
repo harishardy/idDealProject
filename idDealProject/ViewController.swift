18c0b97b1f0baddf38418b0e1503b06c14ccb856
Harish Hardy <mj.harishardy@gmail.com>
2019-06-06 12:59:16 +0530

    Initial Commit

---
+++ idDealProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  idDealProject
+//
+//  Created by harishardy on 06/06/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
